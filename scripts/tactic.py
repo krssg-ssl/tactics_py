2c945d830799d9adfc4496e8f4cc39b84146b508
Mayank Bhushan <miku.bhushan@gmail.com>
2017-05-09 04:52:15 +0530

    Added base class for tactic

---
+++ scripts/tactic.py
@@ -0,0 +1,30 @@
+# Super class for all the tactics
+
+import sys
+sys.path.insert(0,'../../plays.py/scripts')
+
+from abc import ABCMeta, abstractmethod
+from utils import tactics_union
+
+class Tactic(object):
+	# this default time is in milliseconds
+	__metaclass__ = ABCMeta
+
+	DEFAULT_TIMEOUT_PERIOD = 50
+	param = tactics_union.Param()
+	def __init__(self, name, bot_id, time_out=DEFAULT_TIMEOUT_PERIOD):
+		self.name       = name
+		self.bot_id     = bot_id
+		self.time_out   = time_out
+	
+	@abstractmethod
+	def execute(self,state):
+		pass
+
+	@abstractmethod
+	def isComplete(self,state):
+		pass
+
+	@abstractmethod
+	def updateParams(self,state):
+		pass
